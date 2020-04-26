05cf0ca1d3dcdeeeca5d12395210e1f4127ef308
Herman Cheung <herman.kh.cheung@gmail.com>
2020-04-26 13:30:25 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    //element.innerHTML = "Testing";
+  
+    return element;
+}
+  
+document.body.appendChild(component());
